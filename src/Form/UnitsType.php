ad4f356b482aadc018482ddd9b947a851e499d02
benandperez <creoencristo7@hotmail.com>
2022-12-30 03:35:59 -0500

    Subiendo todo...faltando el show

--- src/Form/UnitsType.php
+++ src/Form/UnitsType.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\Amenities;
 use App\Entity\AmenitiesProperty;
 use App\Entity\ConstructionType;
+use App\Entity\ProgrammedActivity;
+use App\Entity\PropertyClass;
+use App\Entity\PropertyType;
+use App\Entity\StatusUnits;
 use App\Entity\TypeKitchen;
 use App\Entity\Units;
 use Doctrine\ORM\EntityRepository;
@@ -18,30 +22,183 @@ class UnitsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('number')
-            ->add('model')
-            ->add('tower')
-            ->add('level')
-            ->add('floor')
-            ->add('modelType')
-            ->add('module')
-            ->add('street')
-            ->add('lotArea')
-            ->add('additionalMeters')
-            ->add('totalLotArea')
-            ->add('closedArea')
-            ->add('openArea')
-            ->add('totalArea')
-            ->add('bedRooms')
-            ->add('closets')
-            ->add('bathrooms')
-            ->add('halfBathrooms')
-            ->add('parkingLots')
-            ->add('observations')
-            ->add('price')
-            ->add('statusUnits')
-            ->add('propertyType')
-            ->add('propertyClass')
+            ->add('statusUnits', EntityType::class, [
+                'class' => StatusUnits::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('su')
+                        ->andWhere('su.status = :status')
+                        ->setParameter('status', '1')
+                        ->orderBy('su.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'label' => 'ESTADO ACTUAL: ',
+                'required' => True,
+                'attr' => [
+                    'class' => 'select',
+                    'placeholder' => 'ESTADO ACTUAL',
+                ],
+            ])
+            ->add('propertyType', EntityType::class, [
+                'class' => PropertyType::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('pt')
+                        ->andWhere('pt.status = :status')
+                        ->setParameter('status', '1')
+                        ->orderBy('pt.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'label' => 'TIPO DE INMUEBLE: ',
+                'required' => True,
+                'attr' => [
+                    'class' => 'select',
+                    'placeholder' => 'TIPO DE INMUEBLE',
+                ],
+            ])
+            ->add('propertyClass', EntityType::class, [
+                'class' => PropertyClass::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('pc')
+                        ->andWhere('pc.status = :status')
+                        ->setParameter('status', '1')
+                        ->orderBy('pc.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'label' => 'CLASE DE INMUEBLE: ',
+                'required' => True,
+                'attr' => [
+                    'class' => 'select',
+                    'placeholder' => 'CLASE DE INMUEBLE',
+                ],
+            ])
+            ->add('number', null, [
+                'required' => True,
+                'label' => 'NUMERO: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'NUMERO',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('model', null, [
+                'required' => True,
+                'label' => '¿ES EL MODELO?: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => '¿ES EL MODELO?',
+                    'class' => 'form-control'
+                ],
+            ])
+
+            ->add('tower', null, [
+                'required' => True,
+                'label' => 'TORRE: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'TORRE',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('level', null, [
+                'required' => True,
+                'label' => 'NIVELPISO Nº: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'NIVELPISO Nº',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('floor', null, [
+                'required' => True,
+                'label' => 'PISO Nº: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'PISO Nº',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('modelType', null, [
+                'required' => True,
+                'label' => 'MODELO: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'MODELO',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('module', null, [
+                'required' => True,
+                'label' => 'MODULO: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'MODULO',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('street', null, [
+                'required' => True,
+                'label' => 'CALLE: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'CALLE',
+                    'class' => 'form-control'
+                ],
+            ])
+
+            ->add('lotArea', null, [
+                'required' => True,
+                'label' => 'AREA DEL LOTE: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'AREA DEL LOTE',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('additionalMeters', null, [
+                'required' => True,
+                'label' => 'METROS ADICIONALES: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'METROS ADICIONALES',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('totalLotArea', null, [
+                'required' => True,
+                'label' => 'TOTAL AREA LOTE: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'TOTAL AREA LOTE',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('closedArea', null, [
+                'required' => True,
+                'label' => 'AREA CERRADA: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'AREA CERRADA',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('openArea', null, [
+                'required' => True,
+                'label' => 'AREA ABIERTA: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'AREA ABIERTA',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('totalArea', null, [
+                'required' => True,
+                'label' => 'TOTAL AREA: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'TOTAL AREA',
+                    'class' => 'form-control'
+                ],
+            ])
+
             ->add('amenitiesProperty', EntityType::class, [
                 'class' => AmenitiesProperty::class,
                 'required' => false,
@@ -64,6 +221,42 @@ class UnitsType extends AbstractType
                     'class' => 'select',
                 ] ,
             ])
+            ->add('bedRooms', null, [
+                'required' => True,
+                'label' => 'RECAMARAS: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'RECAMARAS',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('closets', null, [
+                'required' => True,
+                'label' => 'CLOSETS: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'CLOSETS',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('bathrooms', null, [
+                'required' => True,
+                'label' => 'BAÑOS: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'BAÑOS',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('halfBathrooms', null, [
+                'required' => True,
+                'label' => 'MEDIOS BAÑOS: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'MEDIOS BAÑOS',
+                    'class' => 'form-control'
+                ],
+            ])
             ->add('typeKitchen', EntityType::class, [
                 'class' => TypeKitchen::class,
                 'query_builder' => function (EntityRepository $er) {
@@ -73,13 +266,42 @@ class UnitsType extends AbstractType
                         ->orderBy('ct.name', 'ASC');
                 },
                 'choice_label' => 'name',
-                'label' => 'TIPO DE CONSTRUCCION: ',
+                'label' => 'COCINA: ',
                 'required' => True,
                 'attr' => [
                     'class' => 'select',
-                    'placeholder' => 'TIPO DE CONSTRUCCION',
+                    'placeholder' => 'COCINA',
                 ],
             ])
+            ->add('parkingLots', null, [
+                'required' => True,
+                'label' => 'ESTACIONAMIENTOS: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'ESTACIONAMIENTOS',
+                    'class' => 'form-control'
+                ],
+            ])
+            ->add('price', null, [
+                'required' => True,
+                'label' => 'PRECIO: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'PRECIO',
+                    'class' => 'form-control'
+                ],
+            ])
+
+            ->add('observations', null, [
+                'required' => True,
+                'label' => 'OBSERVACIONES: ',
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => 'OBSERVACIONES',
+                    'class' => 'form-control'
+                ],
+            ])
+
         ;
     }
 
